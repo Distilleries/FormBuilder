39ecd69c431b84868a7ce56e577aebef0a5a34c9
Maxime François <mfrancois@big-boss-studio.com>
2016-01-14 16:41:25 +0100

    Remove choice classes

--- src/views/form/partial/choice_area.blade.php
+++ src/views/form/partial/choice_area.blade.php
@@ -41,7 +41,7 @@
                                     @foreach($choice as $ch)
                                         <label>
                                         {!! Form::checkbox($name.'['.$header['id'].'][]', $ch['id'], in_array($ch['id'],$options['selected'][$header['id']]), [
-                                            'class'=>'icheck selectore-choice-area-js',
+                                            'class'=>'icheck',
                                             'data-checkbox'=>(in_array($ch['id'],$options['selected'][$header['id']]))?'icheckbox_line-blue':'icheckbox_line-grey',
                                             'data-label'=>$ch['libelle'],
                                         ]) !!}
