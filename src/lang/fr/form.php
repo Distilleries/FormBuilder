9459c1fe47d63ba6473fcf1dc3c4478cba41af8f
Sébastien Muller <smuller@big-boss-studio.com>
2016-03-27 19:09:13 +0200

    Add i18n for ISO 'fr'

---
+++ src/lang/fr/form.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'detail'                           => 'Détail',
+    'edit'                             => 'Editer',
+    'loading'                          => 'Chargement ...',
+    'searching'                        => 'Recherche ...',
+    'all'                              => 'Tous',
+    'none'                             => 'Aucun',
+    'to'                               => 'A',
+    'pick_file'                        => 'Choisir fichier',
+    'select_value'                     => 'Sélectionner une valeur',
+    'no_result'                        => 'Aucun résultat',
+    'please_enter_n_or_more_character' => 'Veuillez choisir :number ou plus caractères',
+    'you_can_only_select_n_items'      => 'Vous ne pouvez choisir uniquement :number éléments',
+    'yes'                              => 'Oui',
+    'no'                               => 'Non',
+    'na'                               => 'n/a',
+    'save'                             => 'Enregistrer',
+    'back'                             => 'Retour',
+
+];
