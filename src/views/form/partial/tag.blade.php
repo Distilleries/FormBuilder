3e3ad417ffa4d88a023237189ec11311b2ed894a
Maxime François <mfrancois@big-boss-studio.com>
2017-01-04 18:40:27 +0100

    Update input tag

--- src/views/form/partial/tag.blade.php
+++ src/views/form/partial/tag.blade.php
@@ -1,6 +1,6 @@
 @if ($showLabel and $showField)
     <div {!! $options['wrapperAttrs'] !!}>
-@endif
+        @endif
 
         @if ($showLabel)
             <?php $options['label_attr']['class'] .= ' col-md-3'; ?>
@@ -9,44 +9,45 @@
 
         @if ($showLabel)
             <div class="col-md-4">
-        @endif
-
-            @if ($showField)
-                @if (isset($noEdit) and $noEdit === true)
-                    <?php
-                    if (! empty($options['default_value'])) {
-                        $options['default_value'] = is_string($options['default_value']) ? explode(',', $options['default_value']) : $options['default_value'];
-                    }
-                    ?>
-                    @foreach($options['default_value'] as $tag)
-                        <span class="label label-info">{!! $tag !!} </span>
-                    @endforeach
-                @else
-                    <?php $options['class'] = isset($options['class']) ? $options['class'] . ' tags ' : ' tags '; ?>
-                    {!! Form::input($type, $name, $options['default_value'], $options['attr']) !!}
                 @endif
-            @endif
 
-            @if ($showError and isset($errors))
-                {!! $errors->first(array_get($options, 'real_name', $name), '<div ' . $options['errorAttrs'] . '>:message</div>') !!}
-            @endif
-
-        @if ($showLabel)
-                @if (isset($options['help']))
-                    <span class="help-block">{!! $options['help'] !!}</span>
+                @if ($showField)
+                    @if (isset($noEdit) and $noEdit === true)
+                        <?php
+                        if (! empty($options['default_value'])) {
+                            $options['default_value'] = is_string($options['default_value']) ? explode(',', $options['default_value']) : $options['default_value'];
+                        }
+                        ?>
+                        @foreach($options['default_value'] as $tag)
+                            <span class="label label-info">{!! $tag !!} </span>
+                        @endforeach
+                    @else
+                        <?php $options['class'] = isset($options['class']) ? $options['class'] . ' tags ' : ' tags '; ?>
+                        {!! Form::input($type, $name, $options['default_value'], $options['attr']) !!}
+                    @endif
                 @endif
+
+                @if ($showError and isset($errors))
+                    {!! $errors->first(array_get($options, 'real_name', $name), '<div ' . $options['errorAttrs'] . '>:message</div>') !!}
+                @endif
+
+                @if ($showLabel)
+                    @if (isset($options['help']))
+                        <span class="help-block">{!! $options['help'] !!}</span>
+                    @endif
             </div>
         @endif
 
-@if ($showLabel and $showField)
+        @if ($showLabel and $showField)
     </div>
     @if (empty($noEdit))
         <script type="text/javascript">
             jQuery(document).ready(function () {
-                $('#{{ $name }}').select2({
-                    tags: [{{ empty($options['default_value']) ? '' : $options['default_value'] }}],
+                $('input[name="{{ $name }}"]').select2({
+                    tags: [],
+                    tokenSeparators: [','],
                     @if ( !empty($options['maximumInputLength']))
-                        maximumInputLength: {{ $options['maximumInputLength'] }}
+                    maximumInputLength: {{ $options['maximumInputLength'] }}
                     @endif
                 });
             });
